b24d5d83be4305f5e94344eeb2dfe0fec05f3238
mosestyle <76072822+mosestyle@users.noreply.github.com>
2026-04-01 21:25:49 +0200

    Refactor App component by removing captcha logic
    Removed useAltcha hook and its execution from App component.

--- client/src/apps/features/analysis/index.tsx
+++ client/src/apps/features/analysis/index.tsx
@@ -2,7 +2,6 @@ import React, { lazy, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import { useAltcha } from "@/apps/features/analysis/hooks/useAltcha";
 import PageWrapper from "@/components/layout/PageWrapper";
 import { removeDefaultConsentLink } from "@/lib/consent";
 
@@ -18,11 +17,8 @@ const root = ReactDOM.createRoot(
 );
 
 function App() {
-    const executeCaptcha = useAltcha();
-
     useEffect(() => {
         removeDefaultConsentLink();
-        executeCaptcha();
     }, []);
 
     return <BrowserRouter>
